cc45c23e995a613dddb47784785349b9d88a0d75
luan5454 <luancr4z9@gmail.com>
2023-09-14 22:34:26 -0300

    sobe ai barufão

--- APIExemplo/src/main/java/org/example/Main.java
+++ APIExemplo/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+ @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
---
+++ APIExemplo/src/main/java/org/example/controller/alunosController.java
@@ -0,0 +1,20 @@
+package org.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/aluno")
+
+public class alunosController {
+
+
+    @GetMapping("teste")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String teste(){
+        return "Hello Word";
+    }
+}
